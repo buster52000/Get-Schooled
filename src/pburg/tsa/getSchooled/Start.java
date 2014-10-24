39d5741fec5f84549172e898aaa600655950fe58
buster52000 <buster52000@gmail.com>
2014-10-24 13:14:21 -0400

    Worked a bit on world

--- src/pburg/tsa/getSchooled/Start.java
+++ src/pburg/tsa/getSchooled/Start.java
@@ -5,5 +5,7 @@ public class Start {
 	public static boolean running = true;
 	public static final String READABLE_NAME = "Get Schooled";
 	public static final String LOG_PREFIX = "Get-Schooled";
+	public static final int DEFAULT_UNSCALED_WIDTH = 1440;
+	public static final int DEFAULT_UNSCALED_HEIGHT = 900;
 	
 }
--- src/pburg/tsa/getSchooled/world/World.java
+++ src/pburg/tsa/getSchooled/world/World.java
@@ -1,5 +1,14 @@
 package pburg.tsa.getSchooled.world;
 
+import java.util.ArrayList;
+
 public class World {
 
+	private ArrayList<WorldObject> objects;
+	
+	@SuppressWarnings("unchecked")
+	public World (ArrayList<WorldObject> initialObjs, int worldWidth, int worldHeight) {
+		objects = (ArrayList<WorldObject>) initialObjs.clone();
+	}
+	
 }
